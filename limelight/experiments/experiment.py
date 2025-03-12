b807a5ac5c0dc3da1f2dc513200ddb53baf6997e
TakeYourLife3000 <limarco310@gmail.com>
2025-03-12 12:29:54 +0800

    building experiment

---
+++ limelight/experiments/experiment.py
@@ -0,0 +1,23 @@
+
+
+
+
+class ExperimentManager:
+    """
+    This class is responsible for managing the experiment. It will be responsible for
+
+    Experiment:
+    1. Loading the data
+    2. Building the model
+    3. Training the model
+    4. Evaluating the model
+    """
+    def __init__(self):
+        pass
+
+
+
+
+    def run(self):
+        # GOAL: Run the experiment
+        pass
